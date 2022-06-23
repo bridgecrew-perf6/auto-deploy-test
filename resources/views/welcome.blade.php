79e0d4e4d49fb60385621c47dc337dbcc8c4952a
Ney Gelbcke Junior <juniorgelbcke@gmail.com>
2022-06-23 12:40:38 -0300

    update_4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -433,7 +433,7 @@
                 </svg>
             </div>
 
-            <h1 class="bg-white">Test Edited 33333</h1>
+            <h1 class="bg-white">Test Edited 4</h1>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
